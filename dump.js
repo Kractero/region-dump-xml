530fd431d215e8ee372e27009376eb6110a91cec
Kractero <115837402+Kractero@users.noreply.github.com>
2023-10-07 10:42:09 -0700

    automate regional xml dump

---
+++ dump.js
@@ -0,0 +1,25 @@
+import Pako from "pako";
+import { XMLParser } from "fast-xml-parser";
+import { writeFileSync } from "fs";
+
+async function dump(url, filename) {
+  const response = await fetch(url,
+      {
+      headers: {
+          'User-Agent': "Kractero"
+      }
+  });
+
+  const blob = await response.blob();
+
+  const gzippedData = await blob.arrayBuffer();
+  const inflate = new Pako.Inflate();
+  inflate.push(new Uint8Array(gzippedData), true);
+
+  const xmlText = new TextDecoder().decode(inflate.result);
+  const date = new Date();
+  currentDate.setDate(currentDate.getDate()-1);
+  writeFileSync(`data/${date.toISOString().slice(0, 10)}${filename}.xml`, xmlText);
+}
+
+await dump("https://www.nationstates.net/pages/regions.xml.gz", "-Regions")
