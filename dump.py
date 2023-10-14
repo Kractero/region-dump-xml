5ba4b5494da3bddbc8c79e2db77653864a0057c3
Kractero <kractero@sideroca.com>
2023-10-14 14:31:42 -0700

    Rm old files, strip attr for size, make jsons

--- dump.py
+++ dump.py
@@ -1,45 +1,70 @@
 import requests
+import os
 import gzip
 from io import BytesIO
 from datetime import datetime, timedelta
 import xml.etree.ElementTree as ET
+import json
 
-response = requests.get('https://www.nationstates.net/pages/regions.xml.gz', headers={'User-Agent': 'Kractero'})
+todays_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
 
-if response.status_code == 200:
-    with gzip.GzipFile(fileobj=BytesIO(response.content)) as gzipped_file:
-        xml_text = gzipped_file.read()
-    today_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
-    with open(f'data/{today_date}-Regions.xml', 'wb') as xml_file:
-        xml_file.write(xml_text)
+def element_to_dict(element, approved_keys, list_key):
+    result = {}
+    for child in element:
+        child_data = element_to_dict(child, approved_keys, list_key)
+        if child_data:
+            if child.tag in result:
+                if type(result[child.tag]) is list:
+                    result[child.tag].append(child_data)
+                else:
+                    result[child.tag] = [result[child.tag], child_data]
+            else:
+                result[child.tag] = child_data
+        else:
+            if child.tag in approved_keys:
+                if child.tag == list_key:
+                    embassies_data = []
+                    for sub_child in child:
+                        embassies_data.append(sub_child.text)
+                    result[list_key] = ",".join(str(embassy) for embassy in embassies_data)
+                else:
+                    result[child.tag] = child.text
+    return result
 
-    print('XML file downloaded and saved successfully.')
-else:
-    print(f'Failed to fetch dump from NationStates with status {response.status_code}')
+def download_and_save_xml(url, output_file, approved_keys, iter_str, list_key):
+    response = requests.get(url, headers={'User-Agent': 'Kractero'})
+    if response.status_code == 200:
+        with gzip.GzipFile(fileobj=BytesIO(response.content)) as gzipped_file:
+            xml_text = gzipped_file.read()
+        root = ET.fromstring(xml_text)
+        for iterable in root.iter(iter_str):
+            for element in list(iterable):
+                if element.tag not in approved_keys:
+                    iterable.remove(element)
+        filtered_xml_text = ET.tostring(root)
+        with open(output_file, 'wb') as xml_file:
+            xml_file.write(filtered_xml_text)
+        print(f'XML file downloaded and saved successfully: {output_file}')
 
+        xml_dict = element_to_dict(root, approved_keys, list_key)
+        json_data = json.dumps(xml_dict, indent=4)
+        with open(output_file.replace(".xml", ".json"), 'w') as json_file:
+            json_file.write(json_data)
+        print(f'JSON file downloaded and saved successfully: {output_file}')
+    else:
+        print(f'Failed to fetch dump from NationStates with status {response.status_code}')
 
-response = requests.get('https://www.nationstates.net/pages/nations.xml.gz', headers={'User-Agent': 'Kractero'})
+def main():
+    todays_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+    download_and_save_xml('https://www.nationstates.net/pages/regions.xml.gz', f'data/{todays_date}-RegionsTest.xml', ("NAME", "NUMNATIONS", "DELEGATEVOTES", "DELEGATEAUTH", "LASTUPDATE", "FACTBOOK", "EMBASSIES"), "REGION", "EMBASSIES")
+    download_and_save_xml('https://www.nationstates.net/pages/nations.xml.gz', f'data/{todays_date}-NationsTest.xml', ("NAME", "ENDORSEMENTS", "LASTACTIVITY"), "NATION", "ENDORSEMENTS")
+    previous_date = (datetime.now() - timedelta(days=2)).strftime('%Y-%m-%d')
 
-if response.status_code == 200:
-    with gzip.GzipFile(fileobj=BytesIO(response.content)) as gzipped_file:
-        xml_text = gzipped_file.read()
-    root = ET.fromstring(xml_text)
+    xml_files = os.listdir('data')
 
-    new_root = ET.Element("NATIONS")
-
-    for parent in root.findall('.//NATION'):
-        new_nation = ET.SubElement(new_root, "NATION")
-        for child in parent:
-            if child.tag in ['NAME', 'ENDORSEMENTS']:
-                new_nation.append(child)
-
-    tree = ET.ElementTree(new_root)
-    modified_xml_text = ET.tostring(new_root, encoding='utf-8')
-
-    today_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
-    with open(f'data/{today_date}-Nations.xml', 'wb') as xml_file:
-        xml_file.write(modified_xml_text)
-
-    print('XML file downloaded and saved successfully.')
-else:
-    print(f'Failed to fetch dump from NationStates with status {response.status_code}')
+    for xml_file in xml_files:
+        file_date = xml_file.split('-R')[0]
+        if file_date < previous_date:
+            os.remove(os.path.join('data', xml_file))
+    
+main()
